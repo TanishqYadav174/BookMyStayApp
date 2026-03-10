c1d715bc0056df80a67ddd7f7496ecfe4f5fa652
TanishqYadav174 <ty8099@srmist.edu.in>
2026-03-10 09:09:56 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/HotelBookingApp.java
@@ -0,0 +1,13 @@
+public class HotelBookingApp {
+
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=================================");
+
+    }
+}
